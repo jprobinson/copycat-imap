60a09c824bd37b97c03963d2f8c1da173887ebcb
JP Robinson <john.paul.robinson@gmail.com>
2013-05-06 23:46:01 -0400

    yup

--- copycat.py
+++ copycat.py
@@ -85,7 +85,7 @@ help_message = '''
 Please provide a few tidbits of data to run:
     --from_user         if you
     --from_pw           can't figure
-    --from_host         this out. you
+    --from_host         this out, you
     --to_user           shouldn't be
     --to_pw             running this
     --to_host           script.
