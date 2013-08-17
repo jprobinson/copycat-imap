5d8cbf392fa41628d4d2e7d299766196de7749f1
jprobinson <john.paul.robinson@gmail.com>
2013-08-17 17:14:09 -0400

    base logic

--- src/copycat/copycat.go
+++ src/copycat/copycat.go
@@ -1,31 +1,93 @@
 package copycat
 
+import (
+	// "crypto/tls"
+	"github.com/sbinet/go-imap/go1/imap"
+
+	"sync"
+)
+
+type InboxInfo struct {
+	User string
+	Pw   string
+	Host string
+}
+
+func NewCopyCat(srcInfo InboxInfo, dstInfo InboxInfo, catNum int) (cat *CopyCat, err error) {
+	
+	// connect to imap!
+	
+	return
+}
+
+func NewInboxInfo(id string, pw string, host string) (info InboxInfo, err error) {
+	return 
+}
+
+type Config struct {
+	Source InboxInfo
+	Dest   []InboxInfo
+}
+
 // CopyCat represents a process waiting to copy
 type CopyCat struct {
-	
 	// hold on in case of need for reconnect.
-	user     string
-	password string
-	host     string
-
+	SourceInfo InboxInfo
+	DestInfo   InboxInfo
 
 	dest *imap.Client
 	src  *imap.Client
 }
 
-// Sync will make sure all messages in the dest inbox
-// exist in the source inbox
-func (c *CopyCat) Sync() error {
-	// check message counts
-	
-	// if they're off, find out some clever way to compare the 2 quickly.
-	
-	// as you compare, 
+// Sync will make sure that the dst inbox looks exactly like the src.
+func (c *CopyCat) Sync(wg *sync.WaitGroup) error {
+	defer wg.Done()
+	return Sync(c.src, c.dest)
 }
 
-func (c *CopyCat) ListenAndSync() err {
+// Sync will make sure that the dst inbox looks exactly like the src.
+func Sync(src *imap.Client, dst *imap.Client) (err error) {
+	err = syncAdd(src, dst)
+	if err != nil {
+		return
+	}
+
+	err = syncPurge(src, dst)
+	return
+}
+
+// syncAdd will add any files in src that do not exist in dst.
+func syncAdd(src *imap.Client, dst *imap.Client) error {
+	// for each message UID src...
+
+	// do a UIDSearch on dst
+
+	// if it doesnt exist, do a src.UIDFetch -> dst.UIDStore
+
+	return nil
+}
+
+// syncPurge will remove any messages in dst that do not exist in src.
+func syncPurge(src *imap.Client, dst *imap.Client) error {
+	// for each message UID dst...
+
+	// do a UIDSearch on src
+
+	// if it doesnt exist, do a dst.UIDStore with \Deleted flag
+
+	// expunge at the end
+
+	return nil
+}
+
+func (c *CopyCat) SyncAndIdle(wg *sync.WaitGroup) (err error) {
+	defer wg.Done()
 	
-	// sync
-	
+	err = Sync(c.src, c.dest)
+	if err != nil {
+		return
+	}
+
 	// idle ... sync on any notification...only if we can make sync superfast.
+	return
 }
--- src/main/main.go
+++ src/main/main.go
@@ -1,51 +1,104 @@
 package main
 
 import (
-	"time"
+	"flag"
+	"io/ioutil"
+	"encoding/json"
+	"sync"
+	"log"
+	"os"
 
-	"crypto/tls"
-	"github.com/sbinet/go-imap/go1/imap"
+	"copycat"
 )
-
-// CopyCat represents a process waiting to copy
-type CopyCat struct {
-	
-	// hold on in case of need for reconnect.
-	user     string
-	password string
-	host     string
-
-
-	dest *imap.Client
-	src  *imap.Client
-}
-
-// Sync will make sure all messages in the dest inbox
-// exist in the source inbox
-func (c *CopyCat) Sync() error {
-	// check message counts
-	
-	// if they're off, find out some clever way to compare the 2 quickly.
-	
-	// as you compare, 
-}
-
-func (c *CopyCat) ListenAndSync() err {
-	
-	// sync
-	
-	// idle ... sync on any notification...only if we can make sync superfast.
-}
 
 func main() {
 
-	// cli accepts a host id/pw/host and single dest id/pw/host
+	// cli accepts a host id/pw/host
+	var srcId string
+	flag.StringVar(&srcId, "src-id", "", "The login ID for the source mailbox.")
+
+	var srcPw string
+	flag.StringVar(&srcPw, "src-pw", "", "The login password for the source mailbox.")
+
+	var srcHost string
+	flag.StringVar(&srcHost, "src-host", "", "The imap host for the source mailbox.")
+
+	// and single dest id/pw/host
+	var dstId string
+	flag.StringVar(&dstId, "dst-id", "", "The login ID for the destincation mailbox.")
+
+	var dstPw string
+	flag.StringVar(&dstPw, "dst-pw", "", "The login password for the destincation mailbox.")
+
+	var dstHost string
+	flag.StringVar(&dstHost, "dst-host", "", "The imap host for the destincation mailbox.")
 
 	// or multiple dest inbox by config file
+	var configFile string
+	flag.StringVar(&configFile, "config-file", "", "Location of a config file to pass in source and destination login information. Use --example-config to see the format.")
 
 	// single run or idle and wait
+	var idle bool
+	flag.BoolVar(&idle, "idle", false, "Sync the mailboxes and then idle and wait for updates.")
 
 	// accept log file too
-	
-	// for each dest, spawn a CopyCat
+	var logFile string
+	flag.StringVar(&logFile, "log", "stderr", "Location to write logs to. stderr by default. If set, a HUP signal will handle logrotate.")
+
+	flag.Parse()
+
+	var srcInfo copycat.InboxInfo
+	var dstInfos []copycat.InboxInfo
+
+	if len(configFile) == 0 {
+		// put together info from input
+		var err error
+		srcInfo, err = copycat.NewInboxInfo(srcId, srcPw, srcHost)
+		errCheck(err, "Source Info")
+		
+		var dstInfo copycat.InboxInfo
+		dstInfo, err = copycat.NewInboxInfo(dstId, dstPw, dstHost)
+		errCheck(err, "Destination Info")
+		dstInfos = append(dstInfos, dstInfo)
+
+	} else {
+
+		cFile, err := os.Open(configFile)
+		errCheck(err, "Config File")
+
+		configBytes, err := ioutil.ReadAll(cFile)
+		errCheck(err, "Config File")
+		cFile.Close()
+
+		var config copycat.Config
+		err = json.Unmarshal(configBytes, &config)
+		errCheck(err, "Config File")
+
+	}
+
+	// start the work
+	var cats sync.WaitGroup
+	for catNum, dstInfo := range dstInfos {
+		cat, err := copycat.NewCopyCat(srcInfo, dstInfo, catNum)
+		errCheck(err, "IMAP Connection")
+		cats.Add(1)
+
+		if idle {
+			go cat.SyncAndIdle(&cats)
+
+		} else {
+
+			go cat.Sync(&cats)
+		}
+	}
+
+	cats.Wait()
+
+}
+
+func errCheck(err error, msg string) {
+	if err != nil {
+		log.Printf("Invalid %s: %s", msg, err.Error())
+		os.Exit(1)
+	}
 }
